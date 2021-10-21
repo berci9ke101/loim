4b0b04586e4e357f15da49e7ebe0fba663fed66c
Bertalan <berci9ke101@gmail.com>
2021-10-22 00:12:23 +0200

    COMMITMENT TO DIE

---
+++ menu.c
@@ -0,0 +1,8 @@
+#include "functions.h"
+#include "menu.h"
+
+void init_main_menu(void)
+{
+    draw_rect_char(0, 0, 120, 25, "═║╔╗╚╝");
+}
+
---
+++ menu.h
@@ -0,0 +1,7 @@
+
+#ifndef NAGYHF_MENU_H
+#define NAGYHF_MENU_H
+
+void init_main_menu(void);
+
+#endif //NAGYHF_MENU_H
