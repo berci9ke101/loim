5f449337ed0e03038ea7d57a3e8e0863212788e0
Bertalan <berci9ke101@gmail.com>
2021-10-27 22:03:44 +0200

    COMMITMENT TO DIE

---
+++ game.c
@@ -0,0 +1,9 @@
+#include "game.h"
+
+void game_init(void)
+{
+    if (1 == 1)
+    {
+        ;
+    }
+}
---
+++ game.h
@@ -0,0 +1,6 @@
+#ifndef NAGYHF_GAME_H
+#define NAGYHF_GAME_H
+
+void game_init(void);
+
+#endif //NAGYHF_GAME_H
